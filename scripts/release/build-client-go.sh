b428a19218b12c28a8f16be4517d9acde1285816
Eastarpen <eaststarpen@gmail.com>
2023-08-16 22:11:29 +0800

    For old machine, support static build

--- scripts/release/build-client-go.sh
+++ scripts/release/build-client-go.sh
@@ -9,5 +9,11 @@ cd ../../client-go/ || exit
 
 # This will generate file './ping-charts-client'
 go build
-
 mv client-go "../dist/ping-charts-client-go-${GITHUB_REF_NAME}"
+
+# static build 
+# https://stackoverflow.com/questions/55450061/go-build-with-another-glibc/61812048#61812048
+# https://www.reddit.com/r/golang/comments/pi97sp/what_is_the_consequence_of_using_cgo_enabled0/
+export CGO_ENABLED=0
+go build
+mv client-go "../dist/ping-charts-client-go-static-${GITHUB_REF_NAME}"
